a2f69161af34636b48044b9494c4a3df5c51afbe
Ashton Fei <yunjia.fei@gmail.com>
2020-08-14 22:09:42 +0800

    update background images

--- gas/app.js
+++ gas/app.js
@@ -1,5 +1,5 @@
 const RESPONSE_SHEET_NAME = "Responses"
-const START_IMAGE = "https://images.unsplash.com/photo-1471899236350-e3016bf1e69e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
+const START_IMAGE = "https://images.unsplash.com/photo-1590794244624-676c7cd5e1b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
 const END_IMAGE = "https://images.unsplash.com/photo-1471899236350-e3016bf1e69e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
 const FORM_IMAGES = [
     "https://images.unsplash.com/photo-1508717146309-25a0019d0381?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
