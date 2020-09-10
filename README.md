3020104614f01c51da493d8716697e2c5a83191d
Ashton Fei <yunjia.fei@gmail.com>
2020-09-10 22:11:31 +0800

    update script and instruction

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # GAS-064 Google Form Restriction
 
 ### Description
-This is a Google Form with a restriction question built with Google form text validation and apps script.
+This is a Google Form with a restriction question built with Google form [text validation](https://support.google.com/docs/answer/3378864) and apps script. It uses text validation for the restriction of user input, and uses [onFormSubmit(event trigger)](https://developers.google.com/apps-script/reference/script/form-trigger-builder#onformsubmit) to update the text validation pattern after submission.
+
+### Example 
+* Original text validation pattern is "UUID001|UUID002|UUID003|UUID004".
+* When "UUID001" is used in a submission the script will update the text validation pattern to "UUID002|UUID003|UUID004" with "UUID001" removed.
+* After all valid items are used for submission, the script will create a random string like "asdfasfaslj;lajsdfas90120391230019" as the parttern and update the error message to "no item is available now".
 
 ### Demo From
 * [Google From](https://docs.google.com/forms/d/e/1FAIpQLSfyealiMqDhlkgmTAz-fg1VfZShI6oGeIX79IDJ6RVFqukUfw/viewform)
 * Restriction question title: UUID
-* Valid input: UUID001, UUID002, UUID003, UUID004, UUID005
+* Valid input: UUID001, UUID002, UUID003, UUID004
+
+### Make a copy of the script
+[Make a copy](https://docs.google.com/forms/d/18lVqiBDYNeSGcNnBmnwyS-6q_nWm3kMn55CDFH8pmNo/copy)
 
 ### If you prefer to use clasp
 [GAS-059 How to use clasp](https://github.com/ashtonfei/google-apps-script-projects/tree/GAS-259) - [Watch on YouTube](https://youtu.be/V-oE2OyvTKM)
@@ -15,12 +23,32 @@ This is a Google Form with a restriction question built with Google form text va
 Google From
 
 ### Configurations
+In the file app.gs
+``` javascript
+const TITLE = "UUID" // default title of the restriction question
+const DESCRIPTION = "Please enter your UUID" // default description of the restriction question
+const ITEMS = ["UUID001", "UUID002", "UUID003", "UUID004"] // default valid items of the restriction question
+const ERROR = "Invalid UUID code." // default error message of the restriction question
+const EMPTY_MESSAGE = "No item is available now." // default message of the restriction question when there is no valid item left
+
+const KEY_TITLE = "title" // key name for storing restriction question title
+const KEY_DESCRIPTION = "description" // key name for storing the restrction question description
+const KEY_ITEMS = "items" // key name for storing valid items
+const KEY_ERROR = "error" // key name for storing restriction error message
+const KEY_EMPTY_MESSAGE = "empty" // key name for storing the empty message
+```
 
 ### Screenshots
-* Google From
+* Form addon functions (open settings and create trigger)
+![image](https://user-images.githubusercontent.com/16481229/92741218-f77a4300-f3b0-11ea-846d-cee2373c5a13.png)
+* Restriction question settings
+![image](https://user-images.githubusercontent.com/16481229/92740463-58ede200-f3b0-11ea-9434-919ac2b0de13.png)
+* Restriction question
+![image](https://user-images.githubusercontent.com/16481229/92740806-a0746e00-f3b0-11ea-9aee-ea6e0364baf8.png)
+
 
 
 ### YouTube
-* Check on YouTube [GAS-065 Google Form Restriction]
+* Check on YouTube [GAS-065 Google Form Restriction]()
 * More videos about [Google Apps Script](https://www.youtube.com/playlist?list=PLQhwjnEjYj8Bf_EZDrrcmkB9vcB9Sk3x0)
 
--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const TITLE = "UUID" // default title of the restriction question
+const DESCRIPTION = "Please enter your UUID" // default description of the restriction question
 const ITEMS = ["UUID001", "UUID002", "UUID003", "UUID004"] // default valid items of the restriction question
 const ERROR = "Invalid UUID code." // default error message of the restriction question
-const EMPTY_MESSAGE = "No item is availabel now." // default message of the restriction question when there is no valid item left
+const EMPTY_MESSAGE = "No item is available now." // default message of the restriction question when there is no valid item left
 
 const KEY_TITLE = "title" // key name for storing restriction question title
+const KEY_DESCRIPTION = "description" // key name for storing the restrction question description
 const KEY_ITEMS = "items" // key name for storing valid items
 const KEY_ERROR = "error" // key name for storing restriction error message
+const KEY_EMPTY_MESSAGE = "empty" // key name for storing the empty message
+
+const FUNCTION_NAME = "onFormSubmit"
 
 function onOpen() {
     const ui = FormApp.getUi()
-    const menu = ui.createMenu("Restriction")
-    menu.addItem("Update restrictions", "showSidebar")
+    const menu = ui.createMenu("From Restriction")
+    menu.addItem("Settings", "showSidebar")
+    if ( !isTriggerCreated() ) menu.addItem("Create trigger", "createTrigger")
     menu.addToUi()
-    showSidebar()
 }
 
 const showSidebar = () => {
@@ -20,23 +25,29 @@ const showSidebar = () => {
     let items = JSON.parse(scriptProp.getProperty(KEY_ITEMS))
     if ( items ) items = items.join("\n")
     const title = scriptProp.getProperty(KEY_TITLE)
+    const description = scriptProp.getProperty(KEY_DESCRIPTION)
     const error = scriptProp.getProperty(KEY_ERROR)
+    const emptyMessage = scriptProp.getProperty(KEY_EMPTY_MESSAGE)
     
     const ui = FormApp.getUi()
     const template = HtmlService.createTemplateFromFile("sidebar")
     template.items = items || ITEMS.join("\n")
     template.title = title || TITLE
+    template.description = description || DESCRIPTION
     template.error = error || ERROR
+    template.emptyMessage = emptyMessage || EMPTY_MESSAGE
     template.titles = getTextItemTitles()
-
+    
+    template.url = FormApp.getActiveForm().getPublishedUrl()
+    
     const userInterface = template.evaluate()
         .setHeight(560)
         .setWidth(720)
-        .setTitle("Update restrictions")
+        .setTitle("Settings")
     ui.showSidebar(userInterface)
 }
 
-const updateRestriction = ({title, items, error}) => {
+const updateRestriction = ({title, description, items, error, emptyMessage}) => {
     const uuidItem = getItemByTitle(title)
     if ( !uuidItem ) return `<p class="error">Title "${title}" was not found in the form.<\/p>`
     const pattern = items.map(item=>`^${item}$`).join("|")
@@ -48,12 +59,16 @@ const updateRestriction = ({title, items, error}) => {
     uuidItem
         .asTextItem()
         .setRequired(true)
+        .setHelpText(description)
         .setValidation(validation)
+    
     const scriptProp = PropertiesService.getScriptProperties()
     scriptProp.setProperty(KEY_TITLE, title)
+    scriptProp.setProperty(KEY_DESCRIPTION, description)
     scriptProp.setProperty(KEY_ITEMS, JSON.stringify(items))
     scriptProp.setProperty(KEY_ERROR, error)
-    return "Success"
+    scriptProp.setProperty(KEY_EMPTY_MESSAGE, emptyMessage)
+    return "<p>Success<\/p>"
 }
 
 const getItemByTitle = (title) => {
@@ -66,19 +81,19 @@ const getItemByTitle = (title) => {
 
 const getTextItemTitles = () => {
     const form = FormApp.getActiveForm()
-    const items = form.getItems()
-    const textItems = items.filter(item => item.getType() === FormApp.ItemType.TEXT)
+    const textItems = form.getItems(FormApp.ItemType.TEXT)
     const textItemTitles = textItems.map(item => item.getTitle())
     return textItemTitles
 }
 
 
-const onFormsubmit = (e) => {
-    console.log(e)
+const onFormSubmit = (e) => {
     const scriptProps = PropertiesService.getScriptProperties()
-    const title = scriptProps.getProperty(KEY_TITLE)
-    let items = JSON.parse(scriptProps.getProperty(KEY_ITEMS))
-    let error = scriptProps.getProperty(KEY_ERROR)
+    const title = scriptProps.getProperty(KEY_TITLE) || TITLE
+    let description = scriptProps.getProperty(KEY_DESCRIPTION) || DESCRIPTION
+    let items = JSON.parse(scriptProps.getProperty(KEY_ITEMS)) || ITEMS
+    let error = scriptProps.getProperty(KEY_ERROR) || ERROR
+    let emptyMessage = scriptProps.getProperty(KEY_EMPTY_MESSAGE) || EMPTY_MESSAGE
     
     const response = e. response
     const itemResponses = response.getItemResponses()
@@ -93,14 +108,31 @@ const onFormsubmit = (e) => {
     if ( usedItem ) {
         items = items.filter(item => item != usedItem)
         if (items.length === 0) {
-            items = ["afei-" + Utilities.getUuid() + "-afei"]
-            error = EMPTY_MESSAGE
+            items = [Utilities.base64EncodeWebSafe("afei-" + Utilities.getUuid() + "-afei")]
+            error = emptyMessage
+            description = emptyMessage
         }
-        updateRestriction( {title, items, error} )
+        updateRestriction( {title, description, items, error, emptyMessage} )
     }
 }
 
 
+const isTriggerCreated = () => {
+    const type = ScriptApp.EventType.ON_FORM_SUBMIT
+    const triggers = ScriptApp.getProjectTriggers()
+    const matches = triggers.filter( trigger => trigger.getHandlerFunction() === FUNCTION_NAME && trigger.getEventType() === type)
+    if ( matches.length > 0 ) return matches[0]
+}
 
+
+const createTrigger = () => {
+    const type = ScriptApp.EventType.ON_FORM_SUBMIT
+    if ( !isTriggerCreated() ) {
+        const form = FormApp.getActiveForm()
+        ScriptApp.newTrigger(FUNCTION_NAME).forForm(form).onFormSubmit().create()
+        onOpen()
+        FormApp.getUi().alert("Message", "Trigger has been created.", FormApp.getUi().ButtonSet.OK)
+    }
+}
 
 
