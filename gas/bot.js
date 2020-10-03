ebcad0e7c3c0b086cf4716a5a99c075bf358705f
Ashton Fei <yunjia.fei@gmail.com>
2020-10-03 21:35:41 +0800

    update instructions

--- gas/bot.js
+++ gas/bot.js
@@ -1,8 +1,6 @@
 class Bot {
-    constructor(token, offset, sheet) {
+    constructor(token) {
         this.url = "https://api.telegram.org/bot" + token + "/"
-        this.sheet = sheet
-        this.offsetKey = "offset"
     }
     getProp(key){
         return PropertiesService.getScriptProperties().getProperty(key)
@@ -20,27 +18,6 @@ class Bot {
     }
     replyMessage(chat_id, message_id, message) {
         const url = `${this.url}sendMessage?chat_id=${chat_id}&reply_to_message_id=${message_id}&text=${message}`
-        console.log(url)
         return this.get(url)
-    }
-    getUpdates(){
-        const offset = this.getProp(this.offsetKey)
-        let url = `${this.url}getUpdates`
-        if (offset) url = `${this.url}getUpdates?offset=${offset}`
-        const updates = this.get(url)
-        if (!updates.ok) return null
-        const result = updates.result
-        if (result.length === 0) return null
-        const lastUpdate = result[result.length -1]
-        this.setProp(this.offsetKey, (lastUpdate.update_id + 1).toString())
-        return result
-    }
-    getCommands(){
-        const updates = this.getUpdates()
-        if (!updates) return null
-        return updates.filter(update => {
-            if (!update.channel_post) return false
-            return update.channel_post.text.indexOf("/command") === 0
-        })
     }
 }
