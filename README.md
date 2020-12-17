1f005ffe767503872f77c911b9f9a736619967e5
Ashton Fei <yunjia.fei@gmail.com>
2020-12-17 21:41:11 +0800

    initial release

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-# GAS-073 Submit Google Form from Sheet
+# GAS-074 Dashboard with ChartJS & GAS
 
 ### Description
 
-Convert Zoom meeting transcript to data in Google sheet.
+This is a demo dashboard web app built with chartjs, apps script, and google sheet.
+
+### Dashboard
+
+[Dashboard](https://script.google.com/macros/s/AKfycby4hYw-II8yFoWdYozeWgsT8cDwZ6-KqbOvHZPanJ1kKVQX2sMG/exec)
 
 ### Make a copy of the script
 
-[Make a copy](https://docs.google.com/spreadsheets/d/1EpHsWP-ACgDCPQu16dcXv3IpCTjHURB7ZHzHQcmPsFM/copy)
+[Make a copy](https://docs.google.com/spreadsheets/d/1P7J9WUJcz1Ub6AAHvziZ68ACHAzwzrCtinnt1LYSkwQ/copy)
 
 ### If you prefer to use clasp
 
@@ -18,13 +22,10 @@ Google Sheet
 
 ### Screenshots
 
-Get Google Form Request URL
-![image](https://user-images.githubusercontent.com/16481229/101355349-66dcd180-38d1-11eb-8b52-d9d18cf894b5.png)
-
-Get Google Form Entry IDs.
-![image](https://user-images.githubusercontent.com/16481229/101355418-7fe58280-38d1-11eb-8d1b-adbb1fdbd640.png)
+Dashboard
+![image](https://user-images.githubusercontent.com/16481229/102494952-6c47d200-40b0-11eb-8d5c-938c231db118.png)
 
 ### YouTube
 
-- Check on YouTube [GAS-073 Submit Google Form from Sheet](https://youtu.be/K5GO4QN0XQA)
+- Check on YouTube [GAS-074 Dashboard with ChartJS & GAS](https://youtu.be/K5GO4QN0XQA)
 - More videos about [Google Apps Script](https://www.youtube.com/playlist?list=PLQhwjnEjYj8Bf_EZDrrcmkB9vcB9Sk3x0)
--- app.js
+++ app.js
@@ -1,76 +1,85 @@
-const APP_NAME = "Form App"
-const SN_TO_BE_SUBMITTED = "To Be Submitted"
-const HEADER_STATUS = "Submission Status"
-const SUCCESS = "Success"
-const SEP = ","
+const APP_NAME = "Dashboard (ChartJS & GAS)"
+const SN_DASHBOARD = "Dashboard"
 
 class App {
     constructor() {
-        this.ss = SpreadsheetApp.getActive()
-        this.ws = this.ss.getSheetByName(SN_TO_BE_SUBMITTED) || this.ss.insertSheet(SN_TO_BE_SUBMITTED)
-        this.requestUrl = this.ws.getRange("B1").getValue()
+        this.ws = SpreadsheetApp.getActive().getSheetByName(SN_DASHBOARD)
     }
 
+    getColumnName(i) {
+        return this.ws.getRange(1, i + 1).getA1Notation().slice(0, -1)
+    }
 
-    run() {
-        const values = this.ws.getDataRange().getValues()
-        values.shift()
-        values.shift()
-        const headers = values.shift()
-
-        let indexStatusColumn = headers.indexOf(HEADER_STATUS)
-        indexStatusColumn = indexStatusColumn === -1 ? headers.length : indexStatusColumn
-        headers[indexStatusColumn] = HEADER_STATUS
-
-        values.forEach((value, i) => {
-            const status = value[indexStatusColumn] ? value[indexStatusColumn] : ""
-            value[indexStatusColumn] = status
-            if (status !== SUCCESS) {
-                let params = []
-                headers.forEach((header, i) => {
-                    if (i !== indexStatusColumn) {
-                        const items = value[i].trim().split(SEP)
-                        items.forEach(item => {
-                            item = item.trim()
-                            if (item !== "") params.push(`${header}=${item}`)
-                        })
-                    }
-                })
-                const url = `${this.requestUrl}?${params.join("&")}`
-                const result = this.submit(url)
-                value[indexStatusColumn] = result
+    createDatasets(values, colors, bgColors, datasetColumns) {
+        datasetColumns = datasetColumns.split(",").map(v => v.trim().toUpperCase()).filter(v => v !== "")
+        const [, ...headers] = values.shift()
+        const datasets = []
+        headers.forEach((h, i) => {
+            const columnName = this.getColumnName(i + 1)
+            if (datasetColumns.indexOf(columnName) !== -1) {
+                const data = values.map(v => v[i + 1])
+                const label = h
+                datasets.push({ label, data, backgroundColor: bgColors[i], borderColor: colors[i], borderWidth: 1 })
             }
         })
-
-        values.unshift(headers)
-        this.ws.getRange(3, 1, values.length, values[0].length).setValues(values)
+        const labels = values.map(v => v[0])
+        return { labels, datasets }
     }
 
-    submit(url) {
-        try {
-            const response = UrlFetchApp.fetch(url, { method: "POST" })
-            return SUCCESS
-        } catch (e) {
-            return e.message
+    createChartData([index, type, title, url, sheetName, datasetColumns]) {
+        title = `${index}. ${title}`
+        const ss = SpreadsheetApp.openByUrl(url)
+        if (!ss) return null
+        const ws = ss.getSheetByName(sheetName)
+        if (!ws) return null
+
+        const dataRange = ws.getDataRange()
+        const values = dataRange.getValues()
+        const colors = dataRange.getFontColors()[0].slice(1)
+        const bgColors = dataRange.getBackgrounds()[0].slice(1)
+
+        const { labels, datasets } = this.createDatasets(values, colors, bgColors, datasetColumns)
+
+        const chartData = {
+            type,
+            data: {
+                labels,
+                datasets,
+            },
+            options: {
+                title: {
+                    text: title,
+                    display: title !== ""
+                },
+                aspectRatio: 1,
+            }
         }
+        return chartData
+    }
+
+    getDashboardData() {
+        const values = this.ws.getDataRange().getValues()
+        values.shift()
+        const charts = values.map(v => this.createChartData(v))
+        return charts
     }
 }
 
-function run() {
-    const startTime = new Date().getTime()
-    const ss = SpreadsheetApp.getActive()
-    try {
-        ss.toast("Submitting...", APP_NAME)
-        const app = new App()
-        app.run()
-        const endTime = new Date().getTime()
-        const usedTime = Math.floor((endTime - startTime) / 1000)
-        ss.toast(`Done. Used time in second ${usedTime}.`, APP_NAME + " - Success")
-    } catch (e) {
-        ss.toast(e.message, APP_NAME + " - Error")
-    }
+
+function include(filename) {
+    return HtmlService.createTemplateFromFile(filename).evaluate().getContent()
 }
 
-function onOpen() {
-    SpreadsheetApp.getUi().createMenu(APP_NAME).addItem("Submit", "run").addToUi()
+function doGet() {
+    return HtmlService.createTemplateFromFile("index")
+        .evaluate()
+        .setTitle(APP_NAME)
+        .addMetaTag("viewport", "width=device-width, initial-scale=1.0")
+        .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+}
+
+function getDashboardData() {
+    const app = new App()
+    const data = app.getDashboardData()
+    return JSON.stringify({ charts: data, appName: APP_NAME })
 }
