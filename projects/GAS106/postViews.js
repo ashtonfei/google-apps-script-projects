e414b039c7784c7e2bc57c9fb0ef7643f407a096
Ashton Fei <yunjia.fei@gmail.com>
2024-03-14 15:00:01 +0800

    remove cache time from the backend

--- projects/GAS106/postViews.js
+++ projects/GAS106/postViews.js
@@ -1,11 +1,3 @@
-/** set this to 'true' if you want to reduce
- * some duplicate visits from the same browser in a short time
- */
-const USE_FINGERPRINT = false;
-
-// The time for caching the browser fingerprint, max time is 6 hours
-const FINGERPRINT_CACHE_SECONDS = 30 * 60;
-
 function createKey_(blogId, postId) {
   return `${blogId}/${postId}`;
 }
@@ -16,16 +8,20 @@ function createJsonResponse_(data) {
     .setContent(JSON.stringify(data));
 }
 
-function updatePostViews_(blogId, postId, fingerprint) {
+function updatePostViews_(blogId, postId, fingerprint, mins) {
   const cache = CacheService.getScriptCache();
-  const visited = USE_FINGERPRINT ? cache.get(fingerprint) : false;
+  const seconds = /^\d+$/.test(mins) ? mins * 60 : 0;
+  const maxSeconds = 6 * 60 * 60; // 6 hours is the max cache time
+  const useFingerprint = seconds > 0 && seconds <= maxSeconds && fingerprint;
+  const visited = useFingerprint ? cache.get(fingerprint) : false;
+
   const key = createKey_(blogId, postId);
   const props = PropertiesService.getScriptProperties();
   const currentViews = (props.getProperty(key) || 0) * 1;
   if (visited) return currentViews;
   const views = currentViews + 1;
-  if (USE_FINGERPRINT) {
-    cache.put(fingerprint, "VISITED", FINGERPRINT_CACHE_SECONDS);
+  if (useFingerprint) {
+    cache.put(fingerprint, "VISITED", seconds);
   }
   props.setProperty(key, views);
   return visited ? currentViews || 1 : views;
@@ -36,17 +32,14 @@ function updatePostViews_(blogId, postId, fingerprint) {
  * @param {GoogleAppsScript.Events.DoGet} e
  */
 function doGet(e) {
-  const { postId, blogId, key } = e.parameter;
+  const { postId, blogId, fingerprint, mins } = e.parameter;
   if (!blogId) {
     return;
   }
   if (!postId) {
     return;
   }
-  if (!key) {
-    return;
-  }
-  const views = updatePostViews_(blogId, postId, key);
+  const views = updatePostViews_(blogId, postId, fingerprint, mins);
   return createJsonResponse_({
     blogId,
     postId,
